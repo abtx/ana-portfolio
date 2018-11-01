51307581d89659560f2e08d12ac1d70db29a97f2
Artur Janas <artur.abato.janas@gmail.com>
2018-11-01 13:21:54 +0000

    adds gifs

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -5,10 +5,10 @@ $(document).keyup(function(e) {
   }
 });
 //
-// $(document).ready(function() {
-//   showDetails();
-//   setActiveProject(2);
-// });
+$(document).ready(function() {
+  // showDetails();
+  // setActiveProject(1);
+});
 
 var projects = ['bank', 'mimo', 'nhs', 'social', 'storytelling'];
 var activeProject;
